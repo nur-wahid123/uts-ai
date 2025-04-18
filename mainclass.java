edca25dc04c877c7a7f348a97f05e32575bc6dbd
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2025-04-18 18:31:38 +0700

    first

---
+++ mainclass.java
@@ -0,0 +1,15 @@
+package peetemuan5;
+/*
+        f(x)=15x−x^2 
+Program ini mencoba mencari nilai x yang menghasilkan nilai fitness tertinggi dalam rentang 0 hingga 15.
+*/
+public class Peetemuan5 {
+    
+    public static void main(String[] args) {
+        Population population = new Population();
+        population.initializePopulation();
+        population.startGeneric();
+
+    }
+    
+}
